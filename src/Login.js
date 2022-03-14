bc2c38beffa5126c2240cb7eccce99a1190cbd6b
Mohamed Amine Ben Salem <abdbsalem@gmail.com>
2022-03-14 23:43:17 +0100

    login-front-v1.1

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import "./Login.css";
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const singIn = e =>{
+    e.preventDedault()
+    // firebase login
+  }
+
+  const register = e =>{
+    e.preventDedault()
+    // firebase regester
+  }
+
   return (
     <div className="login">
         <Link to='/'>
@@ -12,15 +25,15 @@ function Login() {
             <h1>Sign-in</h1>
             <form>
                 <h5>E-mail</h5>
-                <input type='text'/>
+                <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
                 <h5>Password</h5>
-                <input type='password'/>
-                <button className='login__signInButton'>Sign In</button>
+                <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+                <button type='submit' className='login__signInButton' onClick={singIn}>Sign In</button>
             </form>
             <p>
                 By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
             </p>
-            <button className='login__registerButton'>Create your Amazon Account</button>
+            <button className='login__registerButton' onClick={register}>Create your Amazon Account</button>
         </div>
     </div>
   )
